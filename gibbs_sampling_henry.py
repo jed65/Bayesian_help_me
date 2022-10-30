576f11ba030ab17532a123c2662e7552be7a6b5c
Henry_W <hw875@bath.ac.uk>
2022-10-30 15:11:34 +0000

    Added Comments

--- gibbs_sampling_henry.py
+++ gibbs_sampling_henry.py
@@ -1,28 +1,34 @@
-from statistics import mean
 import numpy as np
 import math
 import cmath
 import matplotlib.pyplot as plt
 
+#My implementation of a Gibbs Sampler for use withe the Ising Model.
+# It is implemented as 2 Classes.
+
 ######################################################################################################################################################
+
+#This class is where the main computations take place
 class Issing:
 
     def __init__(self,n):
         self.size=n**2
-        self.vertices = []
-        self.blue = []
+        self.vertices = [] #A list that contains the the vertices
+        self.blue = [] #Lists which contains teh blue and red vertices
         self.red = []
-        self.state_history = np.ones(self.size).tolist()
+        self.state_history = np.ones(self.size).tolist() #A vector which conatins the current and history of the states of the vertices.
 
         count = 0
+        #Initialising each vertice
         while count < self.size:
             self.vertices.append(Vertex(count,n))
             count = count + 1
 
-        if n%2 == 1:
+        #Creating the lsit of Blur and Red vertices
+        if n%2 == 1: #Odd case
             self.blue.extend(np.arange(0,self.size,2).tolist())
             self.red.extend(np.arange(1,self.size,2).tolist())
-        else:
+        else: #Even Case
             count = 0
             while count < n:
                 if count%2 == 0:
@@ -34,69 +40,77 @@ class Issing:
                 
                 count = count + 1
 
+    #A functio  which completes a step using gibbs sampling
     def step(self,beta):
 
         out = []
 
+        #First the blue points
         for index in self.blue:
             out.append(self.vertices[index].jump(self,beta))
 
+        #Second the Red
         for index in self.red:
             out.append(self.vertices[index].jump(self,beta))
 
+        #Extend state_history with new states
         self.state_history.extend(out)
-        return(out)
+        return(self.state_history)
 
+    #A function the returns the current state of the vertices
     def current_state(self):
         out = []
         for index in np.arange(0,self.size,1):
             out.append(self.vertices[index].spin)
         return(out)
 
+    #The main function to be called when creating a sample using Gibbs sampling
     def giibbs_sample(self,beta,num_iterations):
         count = 0
-        while count < num_iterations:
+        while count < num_iterations: #Step for each iteration
             self.step(beta)
             count = count + 1
-            if count%(num_iterations/100) == 0:
+            if count%(num_iterations/100) == 0: #Print the completion progress
                 print((count/num_iterations)*100,"%")
         
-        return mean(self.state_history)
+        return np.mean(self.state_history)
 
 
 #############################################################################################################################################
+#A class which contains information and methods for the vertices.
 class Vertex:
 
     def __init__(self,location,n):
-        self.location = location
-        self.spin = 1
-        self.negibours = []
+        self.location = location #Location in the grid
+        self.spin = 1 #Current state
+        self.neighbors = [] #Neighboring points.
 
+        #Setting up the neighboring points
         if location%n != 0: 
-            self.negibours.append(location - 1)
+            self.neighbors.append(location - 1)
         
         if location%n != n-1:
-            self.negibours.append(location + 1)
+            self.neighbors.append(location + 1)
 
         if location >= n:
-            self.negibours.append(location - n)
+            self.neighbors.append(location - n)
 
         if location + 1 <=n*(n-1):
-            self.negibours.append(location + n)
+            self.neighbors.append(location + n)
         
         
-    
+    #Jump the vertex to the next state depending on its neighbors
     def jump(self,grid,beta):
         spin_bias = 0
 
-        for index in self.negibours:
+        #Grabs the spins of neighbors
+        for index in self.neighbors:
             spin_bias = spin_bias + grid.vertices[index].spin
         
-       
+        #Probability of changing state       
         jmp_prob=1/(math.exp(-2*beta*spin_bias) + 1)
 
-        #print(jmp_prob)
-
+        #Grabbing a random number from the uniform distribution between -1,1
         if jmp_prob>np.random.random():
             self.spin = 1
         else:
@@ -107,17 +121,18 @@ class Vertex:
         
 
         
+#Testing out are model
+test = Issing(100) #Creating are Grid
 
-test = Issing(100)
+beta = 0.43
+num_iterations = 10000
 
-beta = 0.44
-<<<<<<< HEAD
-num_iterations = 1000
+#Ploting the output
 plot_y = [1]
 count = 1
 while count <= num_iterations:
     test.step(beta)
-    plot_y.append(mean(test.current_state()))
+    plot_y.append(np.mean(test.current_state()))
     count = count + 1
     if count%(num_iterations/100) == 0:
                 print((count/num_iterations)*100,"%")
@@ -129,11 +144,6 @@ plt.plot(plot_x, plot_y, color = 'Red')
 m_inf = (1-math.sinh(2*beta)**-4)**(1/8)
 
 plt.axhline(m_inf.real)
-plt.axhline(mean(test.state_history), color = 'red')
+plt.axhline(np.mean(test.state_history), color = 'red')
 
 plt.show()
-=======
-
-test.vertices[105].jump(test,beta)
-print(test.giibbs_sample(beta,1000))
->>>>>>> Henry
